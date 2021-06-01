f33eeaa8bff01852d67858af206766830bd41d59
Muhammad Azizi <azizi@hotmail.com.my>
2021-06-02 00:09:59 +0800

    Used router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Router from 'vue-router'
+// import Axios from 'axios'
+
+import Q1 from './pages/Q1Page.vue'
+
+Vue.use(Router)
 
 Vue.config.productionTip = false
 
+const router = new Router({
+  routes: [
+    // { path: '/', component: HomePage, meta: { title: 'Home Page' } },
+    { path: '/Q1', component: Q1, meta: { title: 'Question 1' } },
+    // { path: '/', component: HomePage, meta: { title: 'Home Page' } },
+  ],
+  mode: 'history'
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title
+  next()
+})
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
