df956fc7322c70a4a848094d77af06f21ea1753f
Azizi Aziz <azizi@dsm.com.my>
2021-06-02 21:00:21 +0800

    Initialized q2 page

---
+++ frontend/src/pages/Q2Page.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="q2-main">
+    <input type="number">
+    <dropdown />
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  components: {
+  },
+  data: function() {
+    return {
+    }
+  },
+  props: {
+  },
+  methods: {
+  },
+  async mounted() {
+    this.$axios.get('/currencies');
+  },
+  watch: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.q2-main {
+
+}
+</style>
