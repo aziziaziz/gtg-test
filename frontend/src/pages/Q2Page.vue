436ce98b82ea91c959fc60dcc94f0bddf1900a69
Muhammad Azizi <azizi@hotmail.com.my>
2021-06-03 01:10:40 +0800

    Complete partially

--- frontend/src/pages/Q2Page.vue
+++ frontend/src/pages/Q2Page.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="q2-main">
     <input type="number">
-    <dropdown />
+    <dropdown :items="allCurrencies" itemKey="id" itemDesc="name" />
+    <dropdown :items="allCurrencies" itemKey="id" itemDesc="name" />
   </div>
 </template>
 
@@ -12,6 +13,7 @@ export default {
   },
   data: function() {
     return {
+      allCurrencies: []
     }
   },
   props: {
@@ -19,7 +21,9 @@ export default {
   methods: {
   },
   async mounted() {
-    this.$axios.get('/currencies');
+    this.allCurrencies = (await this.$axios.get('/currencies')).data;
+    // setTimeout(async () => {
+    // }, 3000);
   },
   watch: {
   }
@@ -28,6 +32,33 @@ export default {
 
 <style lang="scss" scoped>
 .q2-main {
+  width: 100%;
+  max-width: 600px;
+  display: flex;
+  
+  > * {
+    margin: 5px;
+    box-sizing: border-box;
+    width: 100%;
+  }
 
+  > input[type=number] {
+    font-size: 18px;
+  }
+}
+
+@media only screen and (max-width: 600px) {
+  .q2-main {
+    flex-direction: column;
+
+    > * {
+      margin: 5px 0;
+    }
+
+    > input[type=number] {
+      min-height: 50px;
+      max-height: 50px;
+    }
+  }
 }
 </style>
