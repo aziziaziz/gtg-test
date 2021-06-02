90b3d93b4a4e02fddc413e262f9ee87e45e68dfe
Muhammad Azizi <azizi@hotmail.com.my>
2021-06-03 01:09:44 +0800

    Complete the dropdown

--- frontend/src/components/Dropdown.vue
+++ frontend/src/components/Dropdown.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="dropdown-main">
-    <div class="selected-box">
-      <div class="selected-key">{{ selected ? selected[itemKey] : '' }}</div>
-      <div class="selected-desc">{{ selected ? selected[itemDesc] : '' }}</div>
+    <div class="selected-box" @focus="dropdownFocus" @blur="dropdownBlur" tabindex="0" ref="box" @keydown="search">
+      <div class="d-key">{{ selected ? selected[itemKey] : '' }}</div>
+      <div class="d-desc">{{ selected ? selected[itemDesc] : '' }}</div>
     </div>
-    <div class="item-list">
 
+    <div class="item-list" ref="dropdownItems" :style="{ display: focus ? '' : 'none' }" id="abc">
     </div>
   </div>
 </template>
@@ -15,9 +15,20 @@
 export default {
   components: {
   },
+  computed: {
+  },
   data: function() {
     return {
-      selected: null
+      selected: null,
+      focus: false,
+      canClose: true,
+
+      currentSearch: '',
+      currentIndex: null,
+      tempSelected: null,
+      tempEl: null,
+
+      timer: null
     }
   },
   props: {
@@ -26,10 +37,99 @@ export default {
     itemDesc: { type: String, default: '' },
   },
   methods: {
+    itemClicked: function(item, el) {
+      this.selected = item;
+      this.focus = false;
+      this.tempEl = el.target.parentNode;
+      this.tempEl.classList.add('d-selected');
+    },
+    dropdownFocus: function() {
+      this.focus = true;
+      if (this.selected) {
+        this.$nextTick(() => {
+          this.tempEl = this.$refs.dropdownItems.querySelector(`#${this.selected[this.itemKey]}`);
+          this.tempEl.classList.add('d-selected');
+          console.log(this.tempEl);
+          console.log(this.tempEl.offsetTop);
+
+          this.$refs.dropdownItems.scrollTo(0, this.tempEl.offsetTop);
+        });
+      }
+    },
+    dropdownBlur: function() {
+      if (this.canClose) {
+        this.focus = false;
+      }
+    },
+    search: function(e) {
+      if (e.key == 'Enter') {
+        this.selected = this.tempSelected;
+        this.tempSelected = null;
+        this.$refs.box.blur();
+      } else {
+        if (this.timer) {
+          clearInterval(this.timer);
+        }
+        this.timer = setInterval(() => {
+          this.currentSearch = '';
+          this.currentIndex = null;
+          clearInterval(this.timer);
+        }, 300);
+
+        this.currentSearch += e.key;
+        var search = this.items.filter(i => i[this.itemDesc].toLowerCase().startsWith(this.currentSearch.toLowerCase()));
+        if (search.length > 0) {
+          if (this.tempEl) {
+            this.tempEl.classList.remove('d-selected');
+          }
+          this.tempSelected = search[0];
+          this.tempEl = this.$refs.dropdownItems.querySelector(`#${search[0][this.itemKey]}`);
+          this.tempEl.classList.add('d-selected');
+          
+          this.$refs.dropdownItems.scrollTo(0, this.tempEl.offsetTop);
+        } else {
+          this.tempSelected = null;
+          if (this.tempEl) {
+            this.tempEl.classList.remove('d-selected');
+          }
+        }
+      }
+    }
   },
   mounted() {
   },
   watch: {
+    items: function(val) {
+      let itemList = this.$refs.dropdownItems;
+
+      val.forEach((v) => {
+        var item = document.createElement('div');
+        item.setAttribute('id', v[this.itemKey]);
+        item.classList.add('d-item');
+        item.addEventListener('mouseup', (e) => { this.itemClicked(v, e) });
+        item.addEventListener('mouseenter', () => {
+          this.canClose = false;
+          this.tempSelected = null;
+          if (this.tempEl) {
+            this.tempEl.classList.remove('d-selected');
+          }
+        });
+        item.addEventListener('mouseleave', () => { this.canClose = true });
+
+        var key = document.createElement('div');
+        key.classList.add('d-key');
+        key.innerHTML = v[this.itemKey];
+
+        var desc = document.createElement('div');
+        desc.classList.add('d-desc');
+        desc.innerHTML = v[this.itemDesc];
+
+        item.appendChild(key);
+        item.appendChild(desc);
+
+        itemList.appendChild(item);
+      })
+    }
   }
 }
 </script>
@@ -37,14 +137,49 @@ export default {
 <style lang="scss" scoped>
 .dropdown-main {
   position: relative;
+  width: 100%;
 
   > .selected-box {
     border: 1px solid rgb(128,128,128);
-    min-height: 25px;
+    min-height: 50px;
+    max-height: 50px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding-left: 10px;
   }
 
   > .item-list {
     max-height: 200px;
+    overflow: auto;
+    position: absolute;
+    left: 0;
+    top: 100%;
   }
+}
+</style>
+
+<style>
+.d-item {
+  border-color: rgb(128,128,128);
+  border-width: 0 1px 1px 1px;
+  border-style: solid;
+  padding: 5px;
+  cursor: pointer;
+}
+
+.d-item:hover, .d-selected {
+  background-color: rgb(58, 148, 42);
+  color: white;
+}
+
+.d-key {
+  font-weight: bold;
+  font-size: 1.2em;
+}
+
+.d-desc {
+  font-weight: lighter;
+  font-size: 0.8em;
 }
 </style>
