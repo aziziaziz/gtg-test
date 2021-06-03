7d6108cdd2744a8d743ca8fd1efaab1a8485bff3
Muhammad Azizi <azizi@hotmail.com.my>
2021-06-03 22:15:29 +0800

    Added Home page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,7 @@ import Axios from 'axios'
 
 import Q1 from './pages/Q1Page.vue'
 import Q2 from './pages/Q2Page.vue'
+import Home from './pages/HomePage.vue'
 
 import Dropdown from './components/Dropdown.vue'
 
@@ -16,7 +17,7 @@ Vue.config.productionTip = false
 
 const router = new Router({
   routes: [
-    // { path: '/', component: HomePage, meta: { title: 'Home Page' } },
+    { path: '/', component: Home, meta: { title: 'Home Page' } },
     { path: '/Q1', component: Q1, meta: { title: 'Question 1' } },
     { path: '/Q2', component: Q2, meta: { title: 'Question 2' } },
   ],
