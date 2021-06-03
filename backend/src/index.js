60c436c7442c5663b465401e1bceda1564b60542
Azizi Aziz <azizi@dsm.com.my>
2021-06-03 17:24:07 +0800

    Add conversion route

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 import cors from 'cors';
 
 import mongoConn from '../modules/mongo.js';
-const currenciesdb = new mongoConn('g2gdb', 'currencies');
+const currenciescol = new mongoConn('g2gdb', 'currencies');
+const conversioncol = new mongoConn('g2gdb', 'conversion');
 
 const app = express();
 
@@ -14,18 +15,18 @@ app.get('/', (req, res) => {
 });
 
 app.get('/currencies', async (req, res) => {
-  let currencies = await currenciesdb.getCollection();
+  let currencies = await currenciescol.getCollection();
   let savedCount = await currencies.find().count();
 
   if (savedCount == 0) {
     await currencies.insertOne({ 'time': new Date().getTime() });
-    await currencies.insertMany(await callAPI());
+    await currencies.insertMany(await getAllCurrencies());
   } else {
     let time = await currencies.find({ time: { $ne: null } }).toArray();
     if (new Date().getTime() - time[0]['time'] > 17280000) {
       await currencies.updateOne({ _id: time[0]['_id'] }, { $set: { time: new Date().getTime() } });
       await currencies.deleteMany({ time: null });
-      await currencies.insertMany(await callAPI());
+      await currencies.insertMany(await getAllCurrencies());
     }
   }
   
@@ -36,7 +37,68 @@ app.get('/currencies', async (req, res) => {
   res.status(200).send(result);
 });
 
-async function callAPI() {
+app.get('/convert', async (req, res) => {
+  let to = req.query.to;
+  let from = req.query.from;
+
+  if (to == from) {
+    res.status(406).send({});
+    return;
+  }
+
+  let query = { // (to = to AND from = from) OR (to = from AND from = to)
+    $or: [
+      {
+        $and: [
+          { 'to': to },
+          { 'from': from }
+        ]
+      },
+      {
+        $and: [
+          { 'to': from },
+          { 'from': to }
+        ]
+      }
+    ]
+  };
+
+  let conversion = await conversioncol.getCollection();
+  let savedConversion = await conversion.find(query);
+
+  let result = null;
+  if (await savedConversion.count() > 0) {
+    result = (await savedConversion.toArray())[0];
+
+    if (new Date().getTime() - result['time'] > 17280000) {
+      let live = await getLiveConversion(to, from);
+
+      await conversion.updateOne({ _id: result['_id'] }, {
+        $set: { 
+          time: new Date().getTime(),
+          to: to,
+          from: from,
+          value: live['value'],
+          reversed: live['reversed']
+        }
+      });
+      
+      result = live;
+    }
+  } else {
+    let live = await getLiveConversion(to, from);
+    await conversion.insertOne(live);
+    result = live;
+  }
+
+  if (result) {
+    res.status(200).send(result);
+  } else {
+    res.status(204).send(null);
+  }
+});
+
+async function getAllCurrencies() {
   console.log('from api');
   let currencies = await axios.get('https://free.currconv.com/api/v7/currencies?apiKey=56874a83837f67e445a9');
 
@@ -50,6 +112,16 @@ async function callAPI() {
     });
   });
   return allResult;
+}
+
+async function getLiveConversion(to, from) {
+  console.log('call live');
+  let convert = await axios.get(`https://free.currconv.com/api/v7/convert?apiKey=56874a83837f67e445a9&q=${from}_${to},${to}_${from}`);
+  
+  let key1 = Object.keys(convert.data.results)[0];
+  let key2 = Object.keys(convert.data.results)[1];
+  
+  return { to: to, from: from, value: convert.data.results[key1]['val'], reversed: convert.data.results[key2]['val'], time: new Date().getTime() };
 }
 
 app.listen('3000', () => {
